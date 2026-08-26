4fc2703c8cde2ca74a94c62aa934378da955cd61
David Wan <170246424+david1534@users.noreply.github.com>
2026-08-26 12:24:24 -0400

    study(draft): arrays-001-pair-sum attempt 3 (3/4)

--- attempt/current.py
+++ attempt/current.py
@@ -29,11 +29,11 @@ def pair_sum_indices(inputArray, target):
     seen = {}
     for index, value in enumerate(inputArray):
         complement = abs(value - target)
-        if value and complement not in seen:
-            seen[complement] = index
-        else:
+        if value in seen: 
             compIndex = seen[value]
             return [compIndex, index]
+        elif value not in seen:
+            seen[complement] = index
     raise NotImplementedError
 
 # input = [7,7,2,5]
